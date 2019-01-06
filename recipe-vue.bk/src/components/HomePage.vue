eaacd530814e62d9d6b4e8ce0316dd0ed663265a
Taylor Earl <soccerdude28844@gmail.com>
2019-01-06 12:23:45 -0700

    Start of new site

---
+++ recipe-vue.bk/src/components/HomePage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <div class="recentRecipes">
+        <div v-for="(data, index) in recipes" :key='index'>
+          {{data.recipe}}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      recipes: [
+        {
+          "recipe": "Alfredo",
+          "time" : "Fast",
+          "cost" : "$10"
+        },
+        {
+          "recipe": "Steak",
+          "time" : "Fast",
+          "cost" : "$20"
+        },
+        {
+          "recipe": "Tacos",
+          "time" : "Fast",
+          "cost" : "$15"
+        },
+        {
+          "recipe": "Stew",
+          "time" : "Slow",
+          "cost" : "$20"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
---
+++ recipe-vue/src/views/Recipes.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container fill-height fluid grid-list-xl>
+    <v-flex md12 lg6>
+      <material-card color="orange" title="All Recipes" text>
+        <v-data-table :headers="headers" :items="items" hide-actions>
+          <template slot="headerCell" slot-scope="{ header }">
+            <span class="font-weight-light text-warning text--darken-3" v-text="header.text"/>
+          </template>
+          <template slot="items" slot-scope="{ index, item }">
+            <td>{{ item.name }}</td>
+            <td>{{ item.description }}</td>
+            <td class="text-xs-right">${{ item.price }}</td>
+            <td class="text-xs-right">{{ item.serves }}</td>
+            <td class="text-xs-right">{{ item.addedBy }}</td>
+          </template>
+        </v-data-table>
+      </material-card>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name: "Alfredo",
+          description: "Super creamy",
+          price: 10,
+          serves: 4,
+          addedBy: "Taylor",
+          id: "123",
+        },
+        {
+          name: "Steak",
+          description: "Super tender",
+          price: 20,
+          serves: 4,
+          addedBy: "Ben",
+          id: "123",
+        },
+        {
+          name: "Soup",
+          description: "Super soupy",
+          price: 5,
+          serves: 4,
+          addedBy: "Cache",
+          id: "123",
+        },
+        {
+          name: "Hamburgers",
+          description: "Super cheesy",
+          price: 14,
+          serves: 4,
+          addedBy: "Tylar",
+          id: "123",
+        },
+      ],
+      headers: [
+        {
+          sortable: true,
+          text: 'Name',
+          value: 'name',
+        },
+        {
+          sortable: false,
+          text: 'Description',
+          value: 'description',
+        },
+        {
+          sortable: true,
+          text: 'Price',
+          value: 'price',
+          align: 'right',
+        },
+        {
+          sortable: false,
+          text: 'Serves',
+          value: 'serves',
+          align: 'right',
+        },
+        {
+          sortable: false,
+          text: 'Added By',
+          value: 'addedBy',
+          align: 'right',
+        },
+      ],
+    };
+  },
+};
+</script>
